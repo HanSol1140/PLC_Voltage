b2c331af48c11feddb836c40f9bd025c06303791
송한솔 <skycriperd@gmail.com>
2023-09-15 14:48:04 +0100

    1111

--- plc_server/server.ts
+++ plc_server/server.ts
@@ -24,15 +24,17 @@ const server = app.listen(PORT, () => {
 });
 
 // 리액트 페이지 접속
-app.use(express.static('build'));
-app.get('/', (req, res) => {
-    res.redirect('//home/nanonix/PLC_Voltage/plc_server/build');
-});
+app.use(express.static('/home/nanonix/PLC_Voltage/plc_server/build'));
+
 
 // 라우터 분리
 app.use('/', ipRoutes);
 app.use('/', scaleRoutes);
 app.use('/', measureRoutes);
+
+// app.get('/', (req, res) => {
+//     res.redirect('/home/nanonix/PLC_Voltage/plc_server/build');
+// });
 
 // 모든경로 index.html로 라우팅(SPA)
 app.get('*', (req, res) => {
@@ -43,7 +45,7 @@ app.get('*', (req, res) => {
     });
 });
 
- 
+
 
 // SetIP.json 메모리에 로드
 const plcIP = IPController.getIP().plcIP;
